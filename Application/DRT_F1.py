2098208ff59f8ff4c1617f4cc63323355492ae8b
Darrian <darrianrtalamantes6@gmail.com>
2025-03-01 13:55:36 -0500

    Streamlit app

--- Application/DRT_F1.py
+++ Application/DRT_F1.py
@@ -2,7 +2,13 @@ import streamlit as st
 from PIL import Image
 
 # Streamlit app title
-st.title("Image Upload App")
+# Fancy title with emojis, color, and larger font
+st.markdown("""
+    <h1 style='text-align: center; color: #10cc1d; font-size: 60px; font-family: "Monaco", monospace;
+    '>Welcome to Maize Watch!</h1>
+    <h3 style='text-align: center; color: #4CAF50;
+    '>Please input Image and Other Information</h3>
+""", unsafe_allow_html=True)
 
 # Upload image file
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
